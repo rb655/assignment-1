dcc102a74f5d9e0373bd812a40f9dbfa7932ce07
rb655 <rohitbhanushali655@gmail.com>
2022-08-24 14:56:51 +0530

    gave stylesheet

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+
+  text: {
+    fontSize: 32,
+    fontWeight: '800',
+    textAlign: 'justify',
+    color: 'yellow',
+    margin: 1,
+  },
+});
 
 export default App;
